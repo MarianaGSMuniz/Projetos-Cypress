5a39af0c6c67f4ee1157cb5a7b757c39b01905d5
Mariana Muniz <maewerton60@gmail.com>
2023-03-21 09:16:00 -0300

    Continucação de elementos personalizados

--- Primeiro Projeto/cypress/integration/3-AluraPic/alurapic.spec.js
+++ Primeiro Projeto/cypress/integration/3-AluraPic/alurapic.spec.js
@@ -1,5 +1,3 @@
-const { expect } = require("chai");
-
 describe('Login e registro de usuarios alura pic', () => {
 
     beforeEach(() => {
@@ -8,26 +6,41 @@ describe('Login e registro de usuarios alura pic', () => {
      })
 
     it('verifica mensagens validacao', () => {
-    cy.contains('a','Register now').click();
-    cy.contains('button', 'Register').click();
-    cy.contains('ap-vmessage', 'Email is required!').should('be.visible');
-    cy.contains('button', 'Register').click();
-    cy.contains('ap-vmessage', 'Full name is required!').should('be.visible');
-    cy.contains('ap-vmessage', 'User name is required!').should('be.visible');
-    cy.contains('ap-vmessage', 'Password is required!').should('be.visible');
+        cy.contains('a', 'Register now').click();
+        cy.contains('button', 'Register').click();
+        cy.contains('ap-vmessage', 'Email is required!').should('be.visible');
+        cy.contains('button', 'Register').click();
+        cy.contains('ap-vmessage', 'User name is required!').should('be.visible');
+        cy.contains('ap-vmessage', 'Password is required!').should('be.visible');
+        cy.contains('ap-vmessage', 'Full name is required!').should('be.visible');
+    })
+
+    it('verifica mensagens de email invalido', () => {
+        cy.contains('a', 'Register now').click();
+        cy.contains('button', 'Register').click();
+        cy.get('input[formcontrolname="email"]').type('jacqueline');
+        cy.contains('ap-vmessage', 'Invalid e-mail').should('be.visible');
+     
+    })
+
+    it('verifica mensagens de senha com menos de 8 caracteres', () => {
+        cy.contains('a', 'Register now').click();
+        cy.contains('button', 'Register').click();
+        cy.get('input[formcontrolname="password"]').type('123');
+        cy.contains('button', 'Register').click();
+        cy.contains('ap-vmessage', 'Mininum length is 8').should('be.visible');
+     
     })
 
     it.only('fazer login valido', () => {
         cy.get('input[formcontrolname="userName"]').type('Flavio');
         cy.get('input[formcontrolname="password"]').type('123');
-        cy.get('button [type="submit]').click();
+        cy.get('button [type="submit"]').click();
         cy.contains('a', '(Logout)').should('be.visible');
     })
 
     it.only('fazer login invalido', () => {
-        cy.get('input[formcontrolname="userName"]').type('Miagy');
-        cy.get('input[formcontrolname="password"]').type('123');
-        cy.get('button [type="submit]').click();
+        cy.login('Mariana')
         cy.on('Windows:alert', (str) => {
                 expect(str).to.equal('Invalid user name or password')
 
