0bb3cadde2298a8a8af879faf797d928de23611d
Mariana Muniz <maewerton60@gmail.com>
2023-03-25 10:52:39 -0300

    Login e Caadastro de usuários WIP

--- Primeiro Projeto/cypress/integration/3-AluraPic/cadastro.spec.js
+++ Primeiro Projeto/cypress/integration/3-AluraPic/cadastro.spec.js
@@ -1,8 +1,8 @@
-describe('Login e registro de usuarios alura pic', () => {
+describe('Cadastro de usuarios alura pic', () => {
 
     beforeEach(() => {
       cy.visit('https://alura-fotos.herokuapp.com')
 
      })
-     
+
     })
---
+++ Primeiro Projeto/cypress/integration/3-AluraPic/login.spec.js
@@ -0,0 +1,8 @@
+describe('Login de usuarios alura pic', () => {
+
+    beforeEach(() => {
+      cy.visit('https://alura-fotos.herokuapp.com')
+
+     })
+     
+    })
