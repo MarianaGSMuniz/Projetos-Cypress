13a25cc91521770a254d5b4f7647efd273d1351b
Mariana Muniz <maewerton60@gmail.com>
2023-03-25 10:50:04 -0300

    Organizando os testes

---
+++ Primeiro Projeto/cypress/integration/3-AluraPic/cadastro.spec.js
@@ -0,0 +1,8 @@
+describe('Login e registro de usuarios alura pic', () => {
+
+    beforeEach(() => {
+      cy.visit('https://alura-fotos.herokuapp.com')
+
+     })
+     
+    })
--- Primeiro Projeto/cypress/integration/Alura-busca.spec.js
+++ Primeiro Projeto/cypress/integration/Alura-busca.spec.js
@@ -5,9 +5,9 @@ describe('alura busca cursos', () => {
     })
 
     it('buscar curso de java', () => {
-        cy.get('#header-barraBusca-form-campoBusca').type('java');
+        cy.get('#header-barraBusca-form-campoBusca').type('Javascript');
         cy.get('.header-barraBusca-form-submit').click();
         cy.get('h4.busca-resultado-nome')
-          .should('contain', 'Formação Java e Orientação a Objetos');
+          .should('contain', 'Formação Aprenda a programar em JavaScript com foco no back-end');
     })
 })
